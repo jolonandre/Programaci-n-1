367ce394ff833e601220fe25b8a9691910693922
jolonandre <jolonandre@gmail.com>
2026-01-28 20:43:05 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout <<"Hello World" << endl;
+    cout <<"Juan Andre Jolon Velasquez" << endl;
+    cout <<"9959 25 6194" << endl;
+    return 0;
+}
